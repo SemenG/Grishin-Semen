2398947ba7f78cff72953e1bed3eda513403c397
SemenG <114438300+SemenG@users.noreply.github.com>
2022-10-18 03:03:12 +0300

    Add files via upload

--- 1585A.cpp
+++ 1585A.cpp
@@ -2,30 +2,34 @@
 #include <vector>
 int main() {
 
-
-
-    int t = 0, n = 0, k = 1;
+    int t = 0, n = 0,k = 1;
+    std::vector <int> a(105);
     std::cin >> t;
-    std::vector  <int> a(101);
-
     for (int i = 0; i < t; i++) {
         std::cin >> n;
         for (int l = 0; l < n; l++) {
             std::cin >> a[l];
         }
-        for (int iN = 0; iN < n; iN++) {
-            if (a[iN] == 0 && a[iN + 1] == a[iN]) {
-                k = -1;
-                break;
-            }
-            if (a[iN] == 1 && a[iN + 1] == 0) {
+
+        for (int m = 0; m < t; m++) {
+            k = 1;
+            if (a[0] == 1) {
                 k++;
             }
-            if (a[iN] == 1 && a[iN] == a[iN + 1]) {
-                 k += 4;
+            for (int j = 1; j < n; j++) {
+                if (a[j] == 1 && a[j - 1] != 1) {
+                    k++;
+                }
+                if (a[j] == 1 && a[j - 1] == 1) {
+                    k += 5;
+                }
+                if (a[j] == 0 && a[j - 1] == 0) {
+                    k = -1;
+                    break;
+                }
             }
+            
         }
-
-        std::cout << k;
+        std::cout << k << '\n';
     }
-}
+}   
