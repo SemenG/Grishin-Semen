db32fe3afba2130d6613781e1d7edde76508986f
SemenG <114438300+SemenG@users.noreply.github.com>
2022-12-20 12:09:16 +0300

    Add files via upload

---
+++ Vector.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "Vector.h"
+
+
+RDec2D operator+(const RDec2D vector1, const RDec2D vector2) {
+	RDec2D result{ vector1.x + vector2.x, vector1.y + vector2.y };
+	return result;
+}
+
+std::ostream& operator<<(std::ostream& a, const RDec2D b) {
+	return a << "{" << b.x << ";" << b.y << "}";
+}
+
+RDec2D operator-(const RDec2D vector1, const RDec2D vector2) {
+	RDec2D result{ vector1.x - vector2.x, vector1.y - vector2.y };
+	return result;
+}
+RDec2D operator*(const RDec2D vector1, const RDec2D vector2) {
+	RDec2D result{ vector1.x * vector2.x, vector1.y * vector2.y };
+	return result;
+}
+RDec2D operator/(const RDec2D vector1, const RDec2D vector2) {
+	RDec2D result{ vector1.x / vector2.x, vector1.y / vector2.y };
+	return result;
+}
+double norm(RDec2D& vector1) {
+	double result{ sqrt(vector1.x * vector1.x + vector1.y * vector1.y) };
+	return result;
+}
+double dot(RDec2D& vector1, RDec2D& vector2) {
+	double result{ sqrt(vector1.x * vector2.x + vector1.y * vector2.y) };
+	return result;
+}
---
+++ Vector.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <iostream>
+
+struct RDec2D {
+	double x = 0.0;
+	double y = 0.0;
+};
+
+RDec2D operator+(const RDec2D vector1, const RDec2D vector2); // +
+std::ostream& operator<<(std::ostream& a, const RDec2D b);    // <<
+RDec2D operator-(const RDec2D vector1, const RDec2D vector2); // -
+RDec2D operator*(const RDec2D vector1, const RDec2D vector2); // *
+RDec2D operator/(const RDec2D vector1, const RDec2D vector2); // /
+double norm(RDec2D& vector1);
+double dot(RDec2D& vector1, RDec2D& vector2);
+
+
